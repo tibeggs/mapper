651f511c2cfcbef1b8ebe19a881ac5033d731113
tibeggs <tibeggs@gmail.com>
2023-03-02 08:20:23 -0500

    reorg

--- src/js/main.js
+++ src/js/main.js
@@ -12,21 +12,22 @@ import { transform, fromLonLat, toLonLat } from 'ol/proj';
 import Feature from 'ol/Feature.js';
 import Point from 'ol/geom/Point.js';
 import { Circle as CircleStyle, Fill, Stroke, Style, Icon, RegularShape, Text as olText } from 'ol/style.js';
-// import { run } from './get_weather.js';
 import crags from '../json/crags.json' assert { type: 'JSON' };;
-// import subcrags from '../json/subcrag.json' assert { type: 'JSON' };;
 import Overlay from 'ol/Overlay.js';
 import { get_current_periods } from "./get_weather.js"
 import ImageWMS from 'ol/source/ImageWMS.js';
 import ImageLayer from 'ol/layer/Image';
 import { containsXY } from 'ol/extent';
 import { prun } from './parsewapi.js'
-
-
 import imgUrl from '../images/climbweathersym.png'
 import blackback from '../images/black_rounded_square_flat64x64.png'
-
 import blueback from '../images/blue_rounded_square_flat64x64.png'
+
+var headerimg = document.getElementById('headimage')
+headerimg.src = imgUrl
+headerimg.style.height = '48px';
+headerimg.style.width = '48px';
+const element = document.getElementById('popup');
 
 // let cragjson = await fetch('https://rapid-poetry-328e.cwmtb.workers.dev/').catch("No Good");
 async function call_worker() {
@@ -46,35 +47,21 @@ async function call_worker() {
   })
 
 }
-var headerimg = document.getElementById('headimage')
-headerimg.src = imgUrl
-headerimg.style.height = '48px';
-headerimg.style.width = '48px';
 
 const cragjson = await call_worker()
-console.log(cragjson[0].forecast.forecastday);
+
+var periods = []
+for (var i in cragjson[0].forecast.forecastday) {
+  periods.push([getDayName(cragjson[0].forecast.forecastday[i].date, 'en-US')]);
+}
+
+var cragsmap = new Map(Object.entries(cragjson));
 
 function getDayName(dateStr, locale) {
   var date = new Date(dateStr);
   return date.toLocaleDateString(locale, { weekday: 'long' });
 }
-var periods = []
-for (var i in cragjson[0].forecast.forecastday) {
-  periods.push([getDayName(cragjson[0].forecast.forecastday[i].date, 'en-US')]);
-}
-console.log(periods);
 
-// const periods = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-var cragsmap = new Map(Object.entries(cragjson));
-
-
-
-
-// var subcragsmap = new Map(Object.entries(subcrags));
-
-
-// cragsmap.forEach(mapper);
 const arr = Array.from(cragsmap, ([key, value]) => ({
   key,
   value,
@@ -89,20 +76,11 @@ const arr = Array.from(cragsmap, ([key, value]) => ({
 console.log(Array.isArray(arr));
 console.log(arr);
 
-const featureminzoom = 5
+const featureminzoom = 7;
 const distanceInput = '90';
 const minDistanceInput = '70';
 const deflonlat = [-76.87397, 39.1666]
-const lonlat2 = [-79.28219, 38.81352]
-const latlong = [38.94656, -78.30231]
-const geom = new Point(fromLonLat(deflonlat));
-const feature = new Feature({ 'geometry': geom, 'size': '20' });
 const styleCache = {};
-
-var FeatureMap = new Map()
-var vectorSource = new VectorSource({
-  // features: features
-})
 
 const map = new olMap({
   // projection: 'EPSG:4326',
@@ -121,25 +99,6 @@ const map = new olMap({
 
 });
 
-function fill_crags(subjectObject) {
-  var subjectSel = document.getElementById("cragjump");
-  subjectObject.forEach((value, key) => {
-    subjectSel.options[parseInt(key) + 1] = new Option(value.crag, value.lnglat);
-  })
-  subjectSel.onchange = function () {
-    //empty Chapters- and Topics- dropdowns
-    //   chapterSel.length = 1;
-    //display correct values
-    change_map_view(subjectSel.value.split(","))
-    // request_weather(arr, subjectSel.value);
-  }
-  $('.cragjump').select2({
-    placeholder: "Jump to Crag",
-    allowClear: true,
-    width: "resolve",
-    height: "resolve"
-  });
-}
 fill_crags(cragsmap);
 
 // function fill_sub_crags(subjectObject) {
@@ -168,7 +127,10 @@ fill_crags(cragsmap);
 
 // fill_sub_crags(subcragsmap);
 
-const element = document.getElementById('popup');
+var FeatureMap = new Map();
+var vectorSource = new VectorSource({
+  // features: features
+})
 
 var clusterSource = new Cluster({
   distance: parseInt(distanceInput, 10),
@@ -209,16 +171,16 @@ const clusters = new VectorLayer({
             font: "12px Sans-Serif",
             text: size.toString(),
             fill: new Fill({
-                  color: 'white',
+              color: 'white',
               //     // font: "bold 48px serif",
-                }),
+            }),
             // stroke: new Stroke({color: outlineColor, width: outlineWidth}),
             offsetX: 30,
             offsetY: -30,
             placement: 'point',
           }),
           // text: new olText({
-            
+
           //   offsetY: -30,
           //   text: [size.toString(), "12px Sans-Serif"],
           //   fill: new Fill({
@@ -238,7 +200,110 @@ const clusters = new VectorLayer({
 });
 map.addLayer(clusters);
 
-function feature_maker(lonlat, image_path, tempf, forecast, areaname, url, isday, maxwind, totalprecip,  pastrain) {
+
+set_default_location().then(function (t) {
+  console.log(t);
+  change_map_view(t)
+})
+
+
+
+// Close the popup when the map is moved
+function get_wi() {
+  var subjectSel = document.getElementById("weatherperiod");
+  let w
+  if (subjectSel.value) {
+    return subjectSel.value
+  }
+  else {
+    return 0
+  }
+}
+
+set_periods(periods);
+
+const popup = new Overlay({
+  element: element,
+  positioning: 'bottom-center',
+  stopEvent: false,
+});
+
+map.addOverlay(popup);
+
+let popover;
+
+// display popup on click
+// map.on('click', popup_show(evt));
+map.on('click', function (evt) {
+  popup_show(evt);
+});
+
+
+// change mouse cursor when over marker
+map.on('pointermove', function (e) {
+  const pixel = map.getEventPixel(e.originalEvent);
+  const hit = map.hasFeatureAtPixel(pixel);
+  if (hit) {
+    map.getTargetElement().style.cursor = 'pointer';
+  }
+  else {
+    map.getTargetElement().style.cursor = '';
+  }
+});
+
+
+
+map.on('movestart', disposePopover);
+
+map.on('moveend', function () {
+  if (map.getView().getZoom() > featureminzoom) {
+    let FeaturesToRemove = new Array();
+    let KeysToRemove = new Array();
+    FeatureMap.forEach((value, key, map1) => {
+      let lnglat = key.split(',');
+      if (!containsXY(map.getView().calculateExtent(), fromLonLat(lnglat)[0], fromLonLat(lnglat)[1])) {
+        FeaturesToRemove.push(value);
+        KeysToRemove.push(key);
+      }
+    }
+    )
+    FeaturesToRemove.forEach(function (x) {
+      vectorSource.removeFeature(x);
+    })
+    KeysToRemove.map(function (x) {
+      FeatureMap.delete(x)
+    })
+    var subjectSel = document.getElementById("weatherperiod");
+    request_weather(arr, get_wi())
+  }
+  else {
+    console.log('clear ', map.getView().getZoom());
+    vectorSource.clear();
+  }
+});
+
+request_weather(arr, 0);
+
+
+
+// START FUNCTIONS
+function fill_crags(subjectObject) {
+  var subjectSel = document.getElementById("cragjump");
+  subjectObject.forEach((value, key) => {
+    subjectSel.options[parseInt(key) + 1] = new Option(value.crag, value.lnglat);
+  })
+  subjectSel.onchange = function () {
+
+    change_map_view(subjectSel.value.split(","))
+  }
+  $('.cragjump').select2({
+    placeholder: "Jump to Crag",
+    allowClear: true,
+    width: "resolve",
+    height: "resolve"
+  });
+}
+function feature_maker(lonlat, image_path, tempf, forecast, areaname, url, isday, maxwind, totalprecip, pastrain) {
   var geom = new Point(fromLonLat(lonlat));
   const day_styles = {
     'true': new Style({
@@ -317,25 +382,26 @@ function feature_maker(lonlat, image_path, tempf, forecast, areaname, url, isday
   };
   var bgc = '#00000000';
   var lc = '#00000000';
-  if (!isNaN(pastrain)){
-    bgc =  '#006ddf'
+  if (!isNaN(pastrain)) {
+    bgc = '#006ddf'
     // lc = 'black'
   }
   return new Feature({
-    'geometry': geom, 'image_path': image_path, 'size': '20', 'Forecast': forecast, 'URL': url, 'AreaName': areaname, 'isday': isday, "TempF": tempf,  'MaxWind':maxwind,
-     'TotalPrecip': totalprecip,'style': [
+    'geometry': geom, 'image_path': image_path, 'size': '20', 'Forecast': forecast, 'URL': url, 'AreaName': areaname, 'isday': isday, "TempF": tempf, 'MaxWind': maxwind,
+    'TotalPrecip': totalprecip, 'style': [
       rain_styles[!isNaN(pastrain)],
-      new Style({image: new RegularShape({
-        fill: new Fill({ color: bgc }),
-        stroke: new Stroke({ color: lc, width: 2 }),
-        points: 4,
-        radius: 28 / Math.SQRT2,
-        radius2: 28,
-        points: 4,
-        angle: 0,
-        scale: [1, 0.5],
-        displacement: [0, -36],
-      }),
+      new Style({
+        image: new RegularShape({
+          fill: new Fill({ color: bgc }),
+          stroke: new Stroke({ color: lc, width: 2 }),
+          points: 4,
+          radius: 28 / Math.SQRT2,
+          radius2: 28,
+          points: 4,
+          angle: 0,
+          scale: [1, 0.5],
+          displacement: [0, -36],
+        }),
       }),
       // new Style({
       //   image: new RegularShape({
@@ -370,6 +436,7 @@ function getLocation() {
     navigator.geolocation.getCurrentPosition(resolve, reject)
   );
 }
+
 async function set_default_location() {
   try {
     const position = await getLocation();
@@ -379,11 +446,6 @@ async function set_default_location() {
 
   }
 }
-// let t = await set_default_location();
-set_default_location().then(function (t) {
-  console.log(t);
-  change_map_view(t)
-})
 
 function change_map_view(t) {
   map.setView(
@@ -394,7 +456,6 @@ function change_map_view(t) {
     )
   )
 }
-
 function is_in_extent(item) {
   return containsXY(map.getView().calculateExtent(), fromLonLat(item.value.lnglat)[0], fromLonLat(item.value.lnglat)[1])
 }
@@ -474,7 +535,7 @@ function call_coords(chunk, wi) {
           }
           console.log(pastrain);
           var feat = feature_maker(fmap['lonlat'], fmap['image_path'], fmap['TempF'], fmap['Forecast'], fmap['AreaName'], fmap['URL'], fmap['isDay'], fmap['maxWind'],
-          fmap['totalPrecip'], pastrain)
+            fmap['totalPrecip'], pastrain)
           feat.setStyle(feat.get('style'));
           let lonlatstr = fmap['lonlat'].toString()
           resolve(add_feature_safe(lonlatstr, feat));
@@ -494,7 +555,6 @@ function call_coords(chunk, wi) {
   )
 }
 
-
 function set_periods(subjectObject) {
   var subjectSel = document.getElementById("weatherperiod");
   subjectSel.innerHTML = subjectObject[0];
@@ -506,28 +566,13 @@ function set_periods(subjectObject) {
     request_weather(arr, subjectSel.value);
   }
 };
-set_periods(periods);
 
-const popup = new Overlay({
-  element: element,
-  positioning: 'bottom-center',
-  stopEvent: false,
-});
-map.addOverlay(popup);
-
-let popover;
 function disposePopover() {
   if (popover) {
     popover.dispose();
     popover = undefined;
   }
 }
-// display popup on click
-// map.on('click', popup_show(evt));
-map.on('click', function (evt) {
-  popup_show(evt);
-});
-
 function popup_show(evt) {
   const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
     return feature;
@@ -543,66 +588,10 @@ function popup_show(evt) {
     // content: "Hello",
     title: feature.get('features')[0].get('AreaName'),
     content: feature.get('features')[0].get('TempF') + " F<br>" + feature.get('features')[0].get('Forecast') +
-    "<br> Max Wind: " + feature.get('features')[0].get('MaxWind') + "mph" +
-    "<br> Tot. Precip: " + feature.get('features')[0].get('TotalPrecip') + "in" +
-     "<br><a href=" + feature.get('features')[0].get('URL') + " target='blank'>" + feature.get('features')[0].get('URL') + "</a>",
+      "<br> Max Wind: " + feature.get('features')[0].get('MaxWind') + "mph" +
+      "<br> Tot. Precip: " + feature.get('features')[0].get('TotalPrecip') + "in" +
+      "<br><a href=" + feature.get('features')[0].get('URL') + " target='blank'>" + feature.get('features')[0].get('URL') + "</a>",
   });
   popover.show();
 }
-
-
-// change mouse cursor when over marker
-map.on('pointermove', function (e) {
-  const pixel = map.getEventPixel(e.originalEvent);
-  const hit = map.hasFeatureAtPixel(pixel);
-  if (hit) {
-    map.getTargetElement().style.cursor = 'pointer';
-    // popup_show(e)
-  }
-  else {
-    map.getTargetElement().style.cursor = '';
-    // disposePopover();
-  }
-  // map.getTargetElement().style.cursor = hit ? 'pointer' : '';
-  // popup_show(e)
-});
-// Close the popup when the map is moved
-function get_wi() {
-  var subjectSel = document.getElementById("weatherperiod");
-  let w
-  if (subjectSel.value) {
-    return subjectSel.value
-  }
-  else {
-    return 0
-  }
-}
-map.on('movestart', disposePopover);
-map.on('moveend', function () {
-  if (map.getView().getZoom() > featureminzoom) {
-    let FeaturesToRemove = new Array();
-    let KeysToRemove = new Array();
-    FeatureMap.forEach((value, key, map1) => {
-      let lnglat = key.split(',');
-      if (!containsXY(map.getView().calculateExtent(), fromLonLat(lnglat)[0], fromLonLat(lnglat)[1])) {
-        FeaturesToRemove.push(value);
-        KeysToRemove.push(key);
-      }
-    }
-    )
-    FeaturesToRemove.forEach(function (x) {
-      vectorSource.removeFeature(x);
-    })
-    KeysToRemove.map(function (x) {
-      FeatureMap.delete(x)
-    })
-    var subjectSel = document.getElementById("weatherperiod");
-    request_weather(arr, get_wi())
-  }
-  else {
-    console.log('clear ', map.getView().getZoom());
-    vectorSource.clear();
-  }
-});
-
-request_weather(arr, 0);
+// BOTTOM
